0daaa383c86d5cd54a53f978548a0c4d674d160b
Migas77 <miguel.belchior@ua.pt>
2024-11-15 12:57:27 +0000

    draw graph by edges with different colors for different number of nodes

--- Assignment_1/exhaustive_search_max_weight_matching.py
+++ Assignment_1/exhaustive_search_max_weight_matching.py
@@ -8,7 +8,7 @@ import pickle
 import math
 
 
-def is_matching(G, matching):
+def is_matching(matching):
     """
     Check if the given set of edges is a matching in the graph G.
 
@@ -19,8 +19,9 @@ def is_matching(G, matching):
     global is_matching_counter
     nodes = set()
     for edge in matching:
-        # contar antes de cada if
-        u,v = edge
+        # Extract edge pair
+        # no need to extract the weights
+        u,v, weight = edge
 
         is_matching_counter += 1
         if u in nodes:
@@ -46,8 +47,8 @@ def exhaustive_search_max_weight_matching(G):
     - max_weight: The total weight of the maximum weighted matching.
     """
     global subsets_counter
+    max_weight_matching = set()
     max_weight = 0
-    max_weight_matching = []
 
     # Get all edges with weights
     edges = G.edges(data="weight")
@@ -55,19 +56,16 @@ def exhaustive_search_max_weight_matching(G):
     # Generate all subsets of edges (with size 1, 2, 3, ..., len(edges))
     for r in range(1, num_edges + 1):
         subsets_counter += math.comb(num_edges, r)
-        for subset in combinations(edges, r):
-            # Extract edge pairs
-            # no need to extract the weights as I'm using nx.is_matching
-            edge_set = {(u, v) for u, v, _ in subset}
+        for edge_subset in combinations(edges, r):
 
             # Check if the subset is a valid matching
-            if is_matching(G, edge_set):
+            if is_matching(edge_subset):
                 # Calculate the total weight of the matching
                 # and update if found maximum weight
-                weight = sum(weight for _, _, weight in subset)
+                weight = sum(weight for _, _, weight in edge_subset)
                 if weight > max_weight:
+                    max_weight_matching = edge_subset
                     max_weight = weight
-                    max_weight_matching = edge_set
 
     return max_weight_matching, max_weight
 
@@ -77,6 +75,8 @@ if __name__ == '__main__':
     is_matching_counter = 0
     headers = ['#Nodes', 'Edge Density', '#Edges (n)', '#Basic Operations', 'Execution Time (s)', '#Configs Tested', 'isExpectedResult']
     results = [] 
+    node_colors = { 4: 'r', 5: 'g', 6: 'b', 7: 'c', 8: 'm', 9: 'y',}
+    num_subset_by_num_edges = defaultdict(defaultdict)
     num_subset_by_edge_density_by_nodes = defaultdict(defaultdict)
     num_ismatching_by_edge_density_by_nodes = defaultdict(defaultdict)
     execution_time_by_edge_density_by_nodes = defaultdict(defaultdict)
@@ -85,7 +85,7 @@ if __name__ == '__main__':
         densidade_0.125: [resultado1, resultado2, ...]
     }
     """
-    for n_nodes in range(4, 9):
+    for n_nodes in range(4, 10):
         for edge_density in [0.125, 0.25, 0.5, 0.75]:
             is_matching_counter = 0
             subsets_counter = 0
@@ -106,41 +106,73 @@ if __name__ == '__main__':
                     0,
                     max_weight == expected_max_weight_matching
                 ])
-                
+                if num_edges > 0:
+                    num_subset_by_num_edges[num_edges] = {
+                        "n_nodes": n_nodes,
+                        "subsets_counter": subsets_counter
+                    }
                 num_subset_by_edge_density_by_nodes[edge_density][n_nodes]= subsets_counter
                 num_ismatching_by_edge_density_by_nodes[edge_density][n_nodes]= is_matching_counter
                 execution_time_by_edge_density_by_nodes[edge_density][n_nodes]= execution_time
 
-                # print("\033c", end="")
+                print("\033c", end="")
                 print(tabulate(results, headers=headers, tablefmt="grid"))
-
+                
     # ---------
     # Plotting |
     # ---------
 
     # Number of Subsets
-    # plt.grid(True)
-    # plt.xlabel('Number of Vertices')
-    # plt.ylabel('Number of Subsets')
-    # for edge_density, results_by_node in num_subset_by_edge_density_by_nodes.items():
-    #     plt.plot(results_by_node.keys(), results_by_node.values(), marker='o')
-    # plt.show()
+    num_subset_by_num_edges = dict(sorted(num_subset_by_num_edges.items()))
+    plt.plot(
+        num_subset_by_num_edges.keys(),
+        [entry["subsets_counter"] for entry in num_subset_by_num_edges.values()],
+        color='k',
+    )
+    
+    handle_by_label = {}
+    for num_edges, entry in num_subset_by_num_edges.items():
+        n_nodes = entry["n_nodes"]
+        label = f'n_nodes={n_nodes}'
+        line, = plt.plot(
+            num_edges, entry["subsets_counter"], marker='o', label=label,
+            color=node_colors[n_nodes]
+        )
+        if label not in handle_by_label:
+            handle_by_label[label] = line
+
+    handle_by_label = dict(sorted(handle_by_label.items()))
+    plt.legend(labels=handle_by_label.keys(), handles=handle_by_label.values())
+    plt.grid(True)
+    plt.xlabel('Number of Edges')
+    plt.ylabel('Number of Subsets')
+    plt.yscale('log')
+    plt.show()
 
     # is_matching counter
     # plt.grid(True)
     # plt.xlabel('Number of Vertices')
     # plt.ylabel('is_matching')
     # for edge_density, results_by_node in num_ismatching_by_edge_density_by_nodes.items():
-    #     plt.plot(results_by_node.keys(), results_by_node.values(), marker='o')
+    #     plt.plot(results_by_node.keys(), results_by_node.values(), marker='o', label=f'Edge Density: {edge_density*100}%')
+    # plt.grid(True)
+    # plt.xlabel('Number of Vertices')
+    # plt.ylabel('is_matching')
+    # plt.yscale('log')
+    # plt.legend()
     # plt.show()
 
+    # draw a graph of log(execution_time) x number of vertices with legends for edge density
     # Execution Time
-    plt.grid(True)
-    plt.xlabel('Number of Vertices')
-    plt.ylabel('Execution Time (s)')
-    for edge_density, results_by_node in execution_time_by_edge_density_by_nodes.items():
-        plt.plot(results_by_node.keys(), results_by_node.values(), marker='o')
-        plt.show()
+    # for edge_density, results_by_node in execution_time_by_edge_density_by_nodes.items():
+    #     # draw the plot but put on the top left screen the edge density
+    #     plt.plot(results_by_node.keys(), results_by_node.values(), marker='o', label=f'Edge Density: {edge_density*100}%')
+    # plt.grid(True)
+    # plt.xlabel('Number of Vertices')
+    # plt.ylabel('Execution Time (s)')
+    # plt.yscale('log')
+    # plt.legend()
+    # plt.show()
     
     
 
