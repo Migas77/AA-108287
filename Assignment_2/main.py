ffed6a11c20031f067397975cafcd9578b8d08f4
Migas77 <miguel.belchior@ua.pt>
2024-12-04 14:27:45 +0000

    random max weighted matching

--- Assignment_2/main.py
+++ Assignment_2/main.py
@@ -1,3 +1,4 @@
+import exp
 import click
 import numpy as np
 from tabulate import tabulate
@@ -7,6 +8,11 @@ import networkx as nx
 from random_max_weighted_matching import random_max_weighted_matching
 from random_max_weighted_matching_with_heuristic import random_max_weighted_matching_with_mix_heuristic
 from probabilistic_greedy_search import probabilistic_greedy_search
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
 
 @click.command()
 @click.option('--algorithm', type=click.Choice(['random', 'random_with_heuristic', 'probabilistic_greedy']), required = True)
@@ -16,10 +22,11 @@ def run_algorithm(algorithm):
     'random_with_heuristic': random_max_weighted_matching_with_mix_heuristic,
     'probabilistic_greedy': probabilistic_greedy_search
   }[algorithm]
-  headers = ['#Nodes', 'Edge Density', '#Edges', '#Subsets Tested', '#is_matching operations', 'Execution Time (s)', 'isExpectedResult', 'Precision']
+  headers = ['#Nodes', 'Edge Density', '#Edges', '#All Solutions', '#Solutions Tested', '#Basic operations', 'Execution Time (s)', 'isExpectedResult', 'Precision']
   results = []
+  node_range = range(4, 1000, 25)
 
-  for n_nodes in range(20, 1000, 25):
+  for n_nodes in node_range:
     for edge_density in [0.125, 0.25, 0.5, 0.75]:
       # Load Graph
       edge_density_str = str(edge_density).replace('.', '')
@@ -29,6 +36,10 @@ def run_algorithm(algorithm):
         if num_edges <= 0:
             continue
         
+        exp.all_solutions_counter = 0
+        exp.filtered_solutions_counter = 0
+        exp.basic_operations_counter = 0
+
         start_time = time.perf_counter()
         max_weight_matching, max_weight = algorithm_func(Graph)
         execution_time = time.perf_counter() - start_time
@@ -39,22 +50,100 @@ def run_algorithm(algorithm):
           n_nodes,
           edge_density,
           num_edges,
-          0,
-          0,
+          exp.all_solutions_counter,
+          exp.filtered_solutions_counter,
+          exp.basic_operations_counter,
           execution_time,
           (max_weight, expected_max_weight, max_weight == expected_max_weight),
           max_weight / expected_max_weight if expected_max_weight != 0 else -1
-          # num_entered
         ])
 
-      # delete output terminal
-      # print("\033c", end="")
-      print(tabulate(results, headers=headers, tablefmt="grid"))
-      # print the number of correct results
-      print(f"Correct results: {sum(1 for _, _, _, _, _, _, (a, b, c), _ in results if c)} of {len(results)}")
-      print(f"Mean Precision: {np.mean([result[-2][0]/result[-2][1] for result in results])}")
+      #print(tabulate(results, headers=headers, tablefmt="grid"))
 
-  import matplotlib.pyplot as plt
+      if algorithm == 'random' and n_nodes == 154 and edge_density == 0.75:
+        # 154, 0.75
+
+        # Print results
+        print(tabulate(results, headers=headers, tablefmt="grid"))
+        print(f"Correct results: {sum(1 for _, _, _, _, _, _, _, (a, b, c), _ in results if c)} of {len(results)}")
+        print(f"Mean Precision: {np.mean([result[-2][0]/result[-2][1] for result in results])}")
+
+        node_colors = { 4: 'tab:blue', 29: 'tab:orange', 54: 'tab:green', 79: 'tab:red', 104: 'tab:purple', 129: 'tab:brown', 154: 'tab:pink'}
+
+        # Plot number of solutions tested
+        number_edges = [result[2] for result in results]
+        number_filtered_solutions_tested = [result[4] for result in results]
+        plt.figure(figsize=(10, 6))
+        for i in range(0, len(number_edges), 4):
+          batch_edges = number_edges[i:i+4]
+          batch_solutions_tested = number_filtered_solutions_tested[i:i+4]
+          plt.scatter(batch_edges, batch_solutions_tested, color=node_colors[node_range[i//4]], label=f'n_nodes = {node_range[i//4]}')
+        plt.xlabel('Number of Edges')
+        plt.ylabel('Number of Solutions Tested')
+        plt.title('Number of Solutions Tested by Number of Edges')
+        plt.legend(loc='lower right')
+        plt.grid(True)
+        plt.show()
+
+        # Plot number of basic operations by number of edges with linear fit
+        number_basic_operations = [result[5] for result in results]
+        plt.figure(figsize=(10, 6))
+        for i in range(0, len(number_edges), 4):
+          batch_edges = number_edges[i:i+4]
+          batch_basic_operations = number_basic_operations[i:i+4]
+          plt.scatter(batch_edges, batch_basic_operations, color=node_colors[node_range[i//4]], label=f'n_nodes = {node_range[i//4]}', zorder=2)
+        all_edges = np.array(number_edges).reshape(-1, 1)
+        all_basic_operations = np.array(number_basic_operations)
+        model = LinearRegression()
+        model.fit(all_edges, all_basic_operations)
+        predicted_operations = model.predict(all_edges)
+        plt.plot(number_edges, predicted_operations, color='k', label=f'Linear Fit: y = {model.coef_[0]:.4f}x + {model.intercept_:.4f}', zorder=1)
+        plt.xlabel('Number of Edges')
+        plt.ylabel('Number of Basic Operations')
+        plt.title('Number of Basic Operations by Number of Edges')
+        plt.legend(loc='upper left')
+        plt.grid(True)
+        plt.show()
+
+        # Plot execution time by number of edges with linear fit
+        execution_times = [result[6] for result in results]
+        plt.figure(figsize=(10, 6))
+        for i in range(0, len(number_edges), 4):
+          batch_edges = number_edges[i:i+4]
+          batch_execution_times = execution_times[i:i+4]
+          plt.scatter(batch_edges, batch_execution_times, color=node_colors[node_range[i//4]], label=f'n_nodes = {node_range[i//4]}', zorder=2)
+        all_edges = np.array(number_edges).reshape(-1, 1)
+        all_execution_times = np.array(execution_times)
+        model = LinearRegression()
+        model.fit(all_edges, all_execution_times)
+        predicted_times = model.predict(all_edges)
+        plt.plot(number_edges, predicted_times, color='k', label=f'Linear Fit: y = {model.coef_[0]:.4f}x + {model.intercept_:.4f}', zorder=1)
+        plt.xlabel('Number of Edges')
+        plt.ylabel('Execution Time (s)')
+        plt.title('Execution Time by Number of Edges')
+        plt.legend(loc='upper left')
+        plt.grid(True)
+        plt.show()
+
+        # Plot precision by number of edges
+        precision = [result[-1] for result in results]
+        plt.figure(figsize=(10, 6))
+        for i in range(0, len(number_edges), 4):
+          batch_edges = number_edges[i:i+4]
+          batch_precision = precision[i:i+4]
+          plt.scatter(batch_edges, batch_precision, color=node_colors[node_range[i//4]], label=f'n_nodes = {node_range[i//4]}')
+        plt.xlabel('Number of Edges')
+        plt.ylabel('Precision')
+        plt.title('Precision by Number of Edges')
+        plt.legend(loc='upper right')
+        plt.grid(True)
+        plt.show()
+
+        return
+
+        
+
+  if algorithm == 'probabilistic_greedy'
 
   # Plot precision
   num_edges = [result[2] for result in results]
--- Assignment_2/random_max_weighted_matching.py
+++ Assignment_2/random_max_weighted_matching.py
@@ -1,9 +1,13 @@
 import time
 import random
+import exp
+
+random.seed(108287)
 
 def random_max_weighted_matching(G, max_iter=10000, time_limit=None):
   start_time = time.perf_counter()
   best_matching = set()
+  shuffled_edges = set()
   edges = list(G.edges(data="weight"))
   total_weight = sum(w for _, _, w in edges)
   max_weight = 0
@@ -12,25 +16,36 @@ def random_max_weighted_matching(G, max_iter=10000, time_limit=None):
     if time_limit and (time.perf_counter() - start_time) > time_limit:
       break
 
+    exp.all_solutions_counter += 1
+
     # Generate a random order of edges
     random.shuffle(edges)
+    tuple_edges = tuple(edges)
+    if (tuple_edges in shuffled_edges):
+      continue
+    shuffled_edges.add(tuple_edges)
+
+    exp.filtered_solutions_counter += 1
     
     # Create the matching greedily by parsing random edges
     current_matching = set()
     matched_vertices = set()
     current_weight = 0
     remaining_weight = total_weight
-    
     for edge in edges:
       u, v, w = edge
       remaining_weight -= w
-      if current_weight + remaining_weight <= max_weight:
+      if current_weight + remaining_weight < max_weight:
         break
-      if u not in matched_vertices and v not in matched_vertices:
-        matched_vertices.add(u)
-        matched_vertices.add(v)
-        current_matching.add(edge)
-        current_weight += w
+      
+      exp.basic_operations_counter += 1
+      if u not in matched_vertices:
+        exp.basic_operations_counter += 1
+        if v not in matched_vertices:
+          matched_vertices.add(u)
+          matched_vertices.add(v)
+          current_matching.add(edge)
+          current_weight += w
 
     # Update best matching if current is better
     if current_weight > max_weight:
@@ -40,3 +55,9 @@ def random_max_weighted_matching(G, max_iter=10000, time_limit=None):
   return best_matching, max_weight
 
 
+"""
+Respostas às dúvidas:
+Usar uma constante para a complexidade do np.random se não se sabe
+Usar 2 counters para os subsets
+O resto está bem
+"""
